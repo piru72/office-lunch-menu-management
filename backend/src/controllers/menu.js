124c37548706d0af53c2c722049ea141a32b4955
piru72 <parvezdirom2000@gmail.com>
2024-05-31 04:15:49 +0600

    add : database connected to POST /menu

--- backend/src/controllers/menu.js
+++ backend/src/controllers/menu.js
@@ -1,9 +1,10 @@
 const { getCurrentTimeWithSeconds } = require('../utils/utils.js');
+const Menu = require('../models/menu.js');
 
 const getMenuOptionsForDate = (req, res) => {
     const requestInboundTime = getCurrentTimeWithSeconds();
     const data = "Template for menu options by date";
-    
+
     res.status(200).json({
         requestInboundTime: requestInboundTime,
         requestOutboundTime: getCurrentTimeWithSeconds(),
@@ -14,20 +15,28 @@ const getMenuOptionsForDate = (req, res) => {
     });
 }
 
-const createMenuOption = (req, res) => {
-    const requestInboundTime = getCurrentTimeWithSeconds();
-    const data = "Template for creating a menu option";
+const createMenuOption = async (req, res) => {
 
-    console.log(req.body);
-    
-    res.status(200).json({
-        requestInboundTime: requestInboundTime,
-        requestOutboundTime: getCurrentTimeWithSeconds(),
-        responseVerdict: "Success",
-        data: {
-            about: data,
-        },
-    });
+    try {
+        const { date, options } = req.body;
+        const requestInboundTime = getCurrentTimeWithSeconds();
+
+        const newMenuOption = await Menu.addMenuOption(date, options);
+
+       
+        res.status(201).json({ 
+            requestInboundTime: requestInboundTime,
+            requestOutboundTime: getCurrentTimeWithSeconds(),
+            responseVerdict: "Menu option added successfully",
+            data: {
+                menuOption: newMenuOption ,
+            }
+        });
+    } catch (error) {
+        
+        console.error('Error adding menu option:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 
 }
 
